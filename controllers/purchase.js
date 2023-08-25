5189f72c0964b70d7b5d0f943a22232997be6c8c
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-08-25 16:55:15 +0530

    razor pay bug fix

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -14,7 +14,7 @@ exports.purchasePremium = (req, res, reject) => {
     try {
         const rzp = new Razorpay({
             key_id: process.env.RAZORPAY_KEY_ID,
-            key_secret: RAZORPAY_KEY_SECRET
+            key_secret: process.env.RAZORPAY_KEY_SECRET
         })
         const amount = process.env.PREMIUM_PURCHASE_COST;
         
